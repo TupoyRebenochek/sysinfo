ac8851750e06b30a9b95165d5d4738141d9a9104
prisoska <46107521+TupoyRebenochek@users.noreply.github.com>
2025-05-09 14:50:17 +0300

    Update install-sysinfo.sh

--- install-sysinfo.sh
+++ install-sysinfo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo wget -O /usr/local/bin/sysinfo https://github.com/TupoyRebenochek/sysinfo/blob/main/sysinfo
+sudo wget -O /usr/local/bin/sysinfo https://raw.githubusercontent.com/TupoyRebenochek/sysinfo/refs/heads/main/sysinfo
 sudo chmod +x /usr/local/bin/sysinfo
 echo "Instalation finished!! Now use 'sysinfo'"
