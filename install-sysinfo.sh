9c9f34cc0d7a3c6f4d53f4ab599dd1a3d389d3fa
prisoska <46107521+TupoyRebenochek@users.noreply.github.com>
2025-05-09 14:47:09 +0300

    Add files via upload

---
+++ install-sysinfo.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo wget -O /usr/local/bin/sysinfo https://raw.githubusercontent.com/TupoyRebenochek/sysinfo/main/sysinfo.sh
+sudo chmod +x /usr/local/bin/sysinfo
+echo "Instalation finished!! Now use 'sysinfo'"
